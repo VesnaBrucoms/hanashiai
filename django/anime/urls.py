cf19ec716cbea999f35d07fd998b68754900eb41
Trevalyan <etstevens60@gmail.com>
2018-03-01 16:47:15 +0000

    Added submission detail pages

--- django/anime/urls.py
+++ django/anime/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'anime'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^search$', views.search, name='search')
+    url(r'^$', views.index,
+        name='index'),
+    url(r'^search$', views.search,
+        name='search'),
+    url(r'^search/(?P<thread_id>[A-z0-9\-\_]+)$', views.thread_detail,
+        name='thread_detail'),
 ]
--- django/anime/views.py
+++ django/anime/views.py
@@ -5,16 +5,19 @@ from hanashiai_app import app_details
 from .forms import SearchForm
 
 
+SUBREDDIT = None
+
+
 def index(request):
     context = {}
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            sr_anime = Subreddit(subreddit='anime',
-                                 app_name=app_details['name'],
-                                 app_version=app_details['version'],
-                                 app_author='HanashiaiDev')
-            results = sr_anime.search(form.cleaned_data['query'])
+            # sr_anime = Subreddit(subreddit='anime',
+            #                      app_name=app_details['name'],
+            #                      app_version=app_details['version'],
+            #                      app_author='HanashiaiDev')
+            results = _get_subreddit().search(form.cleaned_data['query'])
             context['discussions'] = results['discussions']
             context['rewatches'] = results['rewatches']
             return render(request, 'anime/search.html', context)
@@ -25,14 +28,32 @@ def index(request):
 
 def search(request):
     context = {}
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            sr_anime = Subreddit(subreddit='anime',
-                                 app_name=app_details['name'],
-                                 app_version=app_details['version'],
-                                 app_author='HanashiaiDev')
-            results = sr_anime.search(form.cleaned_data['query'])
-            context['discussions'] = results['discussions']
-            context['rewatches'] = results['rewatches']
+    # if request.method == 'POST':
+    #     form = SearchForm(request.POST)
+    #     if form.is_valid():
+    #         sr_anime = Subreddit(subreddit='anime',
+    #                              app_name=app_details['name'],
+    #                              app_version=app_details['version'],
+    #                              app_author='HanashiaiDev')
+    #         results = sr_anime.search(form.cleaned_data['query'])
+    #         context['discussions'] = results['discussions']
+    #         context['rewatches'] = results['rewatches']
     return render(request, 'anime/search.html', context)
+
+
+def thread_detail(request, thread_id):
+    context = {}
+    context['comments'] = _get_subreddit().get_submission_comments(thread_id)
+    return render(request, 'anime/thread_detail.html', context)
+
+
+def _get_subreddit():
+    global SUBREDDIT
+    if SUBREDDIT is None:
+        SUBREDDIT = Subreddit(subreddit='anime',
+                              app_name=app_details['name'],
+                              app_version=app_details['version'],
+                              app_author='HanashiaiDev')
+        return SUBREDDIT
+    else:
+        return SUBREDDIT
