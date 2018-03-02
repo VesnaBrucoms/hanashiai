3266596bb569e98834001e58f38ae13d089722b1
Trevalyan <etstevens60@gmail.com>
2018-03-02 16:00:37 +0000

    Paragraph formatting

--- django/anime/views.py
+++ django/anime/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render
 from hanashiai.core.interfaces import Subreddit
 from hanashiai_app import app_details
@@ -29,10 +31,34 @@ def search(request):
 
 def submission_detail(request, submission):
     context = {}
-    context['sub_title'] = submission.title
-    context['comments'] = _get_subreddit().get_submission_comments(submission)
+    context['sub_title'] = submission.title()
+    comments = _get_subreddit().get_submission_comments(submission)
+    context['comments'] = _parse_comment_text(comments)
     context['column_adj'] = [12, 11, 10, 9, 8, 7, 6, 5]
     return render(request, 'anime/submission_detail.html', context)
+
+
+def _parse_comment_text(comments):
+    # regex = r'(>[\w\d\s\.\,\'\(\)]+\n)'
+    for comment in comments:
+        paragraphs = comment.body.split('\n\n')
+        paragraphs = _parse_quotes(paragraphs)
+        comment.body = paragraphs
+    return comments
+
+
+def _parse_quotes(paragraphs):
+    formatted_paras = []
+    for para in paragraphs:
+        new_text = ''
+        if '>' in para:
+            new_text = para.replace('>', '<p class="text-muted">')
+            new_text += '</p>'
+        else:
+            new_text = '<p>{}'.format(para)
+            new_text += '</p>'
+        formatted_paras.append(new_text)
+    return formatted_paras
 
 
 def _get_subreddit():
